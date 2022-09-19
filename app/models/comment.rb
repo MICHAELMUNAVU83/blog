49e13de3415c1415055e5318be6d5e8f14d9d38e
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-19 15:15:47 +0300

    Made changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: :user_id
-  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+  belongs_to :author
+  belongs_to :post
   def comments_counter
     post.increment!(:comments_counter)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
+after_save :update_post_counter
 class Post < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: :user_id
-  has_many :comments, foreign_key: :post_id
-  has_many :likes, foreign_key: :post_id
+  belongs_to :user, class_name: 'User', foreign_key: :author_id
+  has_many :comments
+  has_many :likes
 
   def last_five_comments
     comments.last(5)
