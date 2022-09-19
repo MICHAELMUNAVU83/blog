fadaf76cdcaac14cb8979fb2168d2a7c49f19926
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-19 10:22:18 +0300

    Added columns

--- db/migrate/20220919071317_create_users.rb
+++ db/migrate/20220919071317_create_users.rb
@@ -1,8 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :photo
+      t.string :bio
+      t.postscounter :postscounter
 
       t.timestamps
-    end
+    ends
   end
 end
