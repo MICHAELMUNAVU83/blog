08d62844963eea178ebe3119cbd1526cb61f6833
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-19 14:27:04 +0300

    Created empty files

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+    belongs_to :user, class_name: 'User', foreign_key: :user_id
+    belongs_to :post, class_name: 'Post', foreign_key: :post_id
+    def comments_counter
+      post.increment!(:comments_counter)
+    end
+end
