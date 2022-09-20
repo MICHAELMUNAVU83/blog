52e072efb1390895c636c379df6a497b27e1040a
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-20 08:42:33 +0300

    Corrected linters

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
-RSpec.describe Post, type: :model do 
-    it "is not valid without a title" do
-        post = Post.new(title: nil)
-        expect(post).to_not be_valid
-      end
-      it "is not valid if title has more than 250 characters" do
-        post = Post.new(title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+RSpec.describe Post, type: :model do
+  it 'is not valid without a title' do
+    post = Post.new(title: nil)
+    expect(post).to_not be_valid
+  end
+  it 'is not valid if title has more than 250 characters' do
+    post = Post.new(title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
         molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
         numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
         optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
@@ -13,10 +13,9 @@ RSpec.describe Post, type: :model do
         nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
         tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
         atis Voluptatem quaerat non architecto ab laudantium
-        modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
-        totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
+        modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
+        totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
         quasi aliquam eligendi, placeat qui corporis!")
-        expect(post).to_not be_valid
-      end
-
+    expect(post).to_not be_valid
+  end
 end
