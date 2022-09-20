745be13a4359b0c2e98fe41565185bedf2749ea3
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-20 08:02:18 +0300

    Added validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
+  validates :name, presence: true 
 
   def last_three_post
     posts.last(3)
