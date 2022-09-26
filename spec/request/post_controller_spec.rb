2a7466a17de46ed7b69c7c0313228efe450fa545
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-26 10:51:34 +0300

    added rspec for post controller

---
+++ spec/request/post_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'Posr Tests', type: :request do
+  context 'Index' do
+    before(:each) do
+      @user = User.create(id: 1, name: 'kim', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                          bio: 'Teacher from Mexico.', postscounter: 8)
+
+      get '/users/1/posts'
+    end
+    it 'Correct template was rendered.' do
+      expect(response).to render_template(:index)
+    end
+    it 'Response status was correct.' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Includes correct placeholder text.' do
+      expect(response.body).to include('This is where all posts are for a single user')
+    end
+  end
+  context 'Show' do
+    before(:each) do
+      @user = User.create(id: 1, name: 'kim', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                          bio: 'Teacher from Mexico.', postscounter: 8)
+      @post = Post.create(id: 1, title: 'Post 1', text: 'This is the post one', postscounter: 8, likescounter: 5,
+                          commentscounter: 7, user_id: 1)
+      get '/users/1/posts/1'
+    end
+    it 'Correct template was rendered' do
+      expect(response).to render_template(:show)
+    end
+    it 'Response status was correct.' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Includes correct placeholder text.' do
+      expect(response.body).to include('This is a single post for a certain user')
+    end
+  end
+end
