dfc94f2c058bd18d2308b532fefd951789c24e88
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-26 10:30:06 +0300

    Tested user controller

---
+++ spec/request/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Users Tests', type: :request do
+  context 'Index' do
+    before(:each) do
+      get '/users'
+    end
+    it 'Correct template was rendered.' do
+      expect(response).to render_template(:index)
+    end
+    it 'Response status was correct.' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Includes correct placeholder text.' do
+      expect(response.body).to include('This is for all users')
+    end
+  end
+  context 'Show' do
+    before(:each) do
+      @user = User.create(id: 1, name: 'kim', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                          bio: 'Teacher from Mexico.', postscounter: 8)
+      get '/users/1'
+    end
+    it 'Correct template was rendered' do
+      expect(response).to render_template(:show)
+    end
+    it 'Response status was correct.' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Includes correct placeholder text.' do
+      expect(response.body).to include('This is a single user')
+    end
+  end
+end
