4322914e114c2164e85095c9e1d93d1a86d554ac
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-28 07:43:09 +0300

    added comment for current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+    def new
+        @comment = Comment.new
+        render :new
+    end
+    def create
+        post = Post.find(params[:post_id])
+        comment = Comment.new(comment_params)
+        comment.user = current_user
+        comment.post = post
+        if comment.save
+            redirect_to user_posts_path(current_user, comment)
+        else
+            render :new
+        end
+    end
+    def comment_params
+        params.require(:comment).permit(:text, :user_id, :post_id)
+    end
 end
