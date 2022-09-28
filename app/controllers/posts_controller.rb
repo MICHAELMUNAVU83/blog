bf1ae83d8388e41c3d1d99610e37af9d0f3e2c9c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-28 07:17:14 +0300

    Added a new post for current users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,23 +7,25 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
   def new
-    @user = User.find(params[:user_id])
     @post = Post.new
+    render :new
   end
+
   def create
-    @post = Post.new(post_params)
-    @post.user_id = params[:user_id]
-    if @post.save
-      redirect_to user_posts_path
+    post = Post.new(post_params)
+    post.user = current_user
+    if post.save
+      redirect_to user_posts_path(current_user, post)
     else
       render :new
     end
   end
 
   private
-  def post_params
-    params.require(:post).permit(:text, :title, :user_id , :postscounter , :likescounter , :commentscounter)
-  end
 
+  def post_params
+    params.require(:post).permit(:text, :title, :user_id, :postscounter, :likescounter, :commentscounter)
+  end
 end
