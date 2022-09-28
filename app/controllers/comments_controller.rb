7efacc24a96d80507f0fc63d3b8295c6311d111c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-28 10:41:05 +0300

    Corrected errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,32 @@
 class CommentsController < ApplicationController
-  def create
-    post = Post.find(params[:post_id])
-    comment = Comment.new(comment_params)
-    comment.user = current_user
-    comment.post = post
-    if comment.save
-      redirect_to user_post_path(current_user, comment)
-    else
-      render :new
+  def new
+    @comment = Comment.new
+    respond_to do |format|
+      format.html { render :new, locals: { comment: @comment } }
     end
   end
 
   def comment_params
-    params.require(:comment).permit(:text, :user_id, :post_id)
+    params
+      .require(:comment)
+      .permit(:text)
+      .merge(user: current_user, post_id: params.require(:post_id))
   end
+
+  def create
+    @comment = Comment.new(comment_params)
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          flash[:success] = 'Comment saved successfully'
+          redirect_to user_post_path(current_user, @comment.post)
+        else
+          flash.now[:error] = 'Error: Comment could not be saved'
+          redirect_to new_post_comment_path(current_user)
+        end
+      end
+    end
+  end
+
+  private :comment_params
 end
