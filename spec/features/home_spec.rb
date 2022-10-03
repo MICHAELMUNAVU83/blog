2c8ddcd8723f63b46d1129c260413b35030f8391
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-03 10:16:03 +0300

    Added tests for user index

--- spec/features/home_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "Home features" do
-    it"should have a home page" do
-        visit root_path
-        expect(page).to have_content("Welcome to the home page")
-    end
-end
