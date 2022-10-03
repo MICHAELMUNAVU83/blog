dfc4f111f56f2be4c9ae59b1f923fe10a34da76e
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-03 07:52:51 +0300

    Refractored code to allow for all users to add posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,9 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
+    @user = User.find(params[:user_id])
 
-    like = current_user.likes.new(
-      user_id: current_user.id,
-      post_id: @post.id
-    )
+   like = Like.new(user: @user, post: @post)
 
     if like.save
       redirect_to "/users/#{@post.user_id}/posts/#{@post.id}", notice: 'Success!'
