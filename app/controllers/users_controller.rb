792cc346d1979692be0fd11f436c47cd38d10a79
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-03 21:03:15 +0300

    Added routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,23 +7,4 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to users_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :bio, :photo, :postscounter)
-  end
 end
